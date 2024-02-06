31337d750571b3e45397ef39eef2aced97b67826
HenriqueGMen <henrimeen24@gmail.com>
2024-02-06 10:58:09 -0300

    :sparkles: feat: Stormglass client

---
+++ src/clients/__test__/stormglass.test.ts
@@ -0,0 +1,20 @@
+import { StormGlass } from '@src/clients/StormGlass';
+import axios from 'axios';
+import stormglassWeatherFixture from '@test/fixtures/stormglass_weather.json';
+import stormglassNormalizedFixture from '@test/fixtures/stormglass_normalized.json'
+
+jest.mock('axios');
+
+describe('Stromglass client', () => {
+  it('should return the normalized forecast from the stormglass service', async () => {
+    const lat = -8.52549;
+    const log = -35.007;
+
+    axios.get = jest.fn().mockResolvedValue(stormglassWeatherFixture);
+
+    const stormGlass = new StormGlass(axios);
+    const res = await stormGlass.fetchPoints(lat, log);
+
+    expect(res).toEqual(stormglassNormalizedFixture);
+  });
+});
