725a7ead6a76ad41050f521dde06fa7856b33e2c
HenriqueGMen <henrimeen24@gmail.com>
2024-03-18 18:52:50 -0300

    att beaches e2e tests

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,8 @@
+import { Beach } from "@src/model/beach";
+
 describe('Beaches functional tests', () => {
+  beforeAll(async () => await Beach.deleteMany());
+
   describe('When a beach is creating', () => {
     it('should be able to create a beach', async () => {
       const beach = {
@@ -13,5 +17,39 @@ describe('Beaches functional tests', () => {
       expect(response.body).toEqual(expect.objectContaining(beach));
     });
 
-  })
+    it('should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+      });
+    });
+
+    it('should return 500 when there is any error other than validation error', async () => {
+      jest.spyOn(Beach.prototype, 'save').mockImplementationOnce(() => {
+        throw new Error('Some unexpected error occurred');
+      });
+    
+      const newBeach = {
+        lat: -33.792726,
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+    
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+    
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual({ error: 'Internal Server Error' });
+    });
+
+  });
 })
